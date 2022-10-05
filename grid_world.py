77ecfa0981fe37e25ae896713466da3d8e7b4250
Ava Abderezaei <ava@MacBook-Pro-4.local>
2022-10-04 23:57:50 -0600

    Add partial iterative policy evaluation

--- grid_world.py
+++ grid_world.py
@@ -6,10 +6,17 @@ class GridWorld:
         T = 'Trap'
         O = 'Obstacle'
     class Action:
-        U = 'Up'
-        D = 'Down'
-        R = 'Right'
-        L = 'Left'
+        U = (-1, 0)
+        D = (1, 0)
+        R = (0, 1)
+        L = (0, -1)
+        
+        NOT_INTENDED = {
+            U: [R, L],
+            D: [R, L],
+            R: [U, D],
+            L: [U, D]
+        }
 
     def __init__(self) -> None:
         self.rewards = {
@@ -22,3 +29,57 @@ class GridWorld:
                      [self.Cell.W, self.Cell.O, self.Cell.W, self.Cell.T],
                      [self.Cell.W, self.Cell.W, self.Cell.W, self.Cell.W],
                      [self.Cell.W, self.Cell.S, self.Cell.W, self.Cell.W]]
+        self.row_size =  len(self.map)
+        self.column_size = len(self.map[0])
+    
+    def reward(self, action, state):
+        cell_type = self.map[state[0]][state[1]]
+        return self.rewards[cell_type]
+
+    def get_successor_states_for_trans_prob(self, current_state, action):
+        next_states = []
+        if self.map[current_state[0]][current_state[1]] in [self.Cell.G, self.Cell.T]:
+            return next_states
+
+        state_intended = (current_state[0] + action[0], current_state[1] + action[1])
+        next_states.append(state_intended)
+        actions_not_intended = self.Action.NOT_INTENDED[action]
+        for not_int_a in actions_not_intended:
+            state_not_intended = (current_state[0] + not_int_a[0], current_state[1] + not_int_a[1])
+            next_states.append(state_not_intended)
+        
+        # make sure indexes are right 
+        for idx, next_st in enumerate(next_states):
+            row = next_st[0]
+            column = next_st[1]
+            if self.map[row][column] == self.Cell.O:
+                next_states[idx] = current_state
+                continue
+            if row == -1:
+                row += 1
+            elif row == self.row_size:
+                row += -1
+            if column == -1:
+                column += 1
+            elif column == self.column_size:
+                column += -1
+            next_states[idx] = (row, column)
+        return next_states        
+
+    def get_successor_states(self, current_state, action):
+        next_states = self.get_successor_states_for_trans_prob(current_state, action)
+        return list(set(next_states))
+    
+    def transaction_probability(self, current_state, next_state, action):
+        p = 0.7  # probability to get into the intended state
+        q = 1 - p
+
+        state_intended = (current_state[0] + action[0], current_state[1] + action[1])
+        next_states = self.get_successor_states_for_trans_prob(current_state, action)
+        states_not_intended = [st for st in next_states if st != state_intended]
+        if state_intended
+
+
+        # next_state_count = next_states.count(next_state)
+        # prob = next_state_count/len(next_states)
+        # return prob
--- iterative_policy_evaluation.py
+++ iterative_policy_evaluation.py
@@ -1,5 +1,54 @@
+
 class PolicyEvaluation:
-    def __init__(self, policy, gridworld) -> None:
-        pass
+
+    def __init__(self, grid, policy, actions) -> None:
+        self.policy = policy
+        self.grid = grid
+        self.actions = actions
+    
+    def init_value_func(self):
+        value_func = []
+        for idx, _ in enumerate(self.grid.map):
+            value_row = []
+            for _ in self.grid.map[idx]:
+                value_row.append(0)
+            value_func.append(value_row)
+        return value_func
+                
+    def successor_states(self, state): pass
+
     def evaluate_policy(self):
-        pass
+        value_function = self.init_value_func()
+        k = 0
+        while k < 10:
+
+            for row in range(self.grid.row_size):
+                for column in range(self.grid.column_size):
+                    state = (row, column)
+                    
+                    v_s = 0
+                    for action in self.actions:
+                        successor_states = self.grid.get_successor_states(state, action) # the successor states if I take action action in state state
+
+                        inner_sum = 0
+                        for next_state in successor_states:
+                            p_s_sp_a = self.grid.transaction_probability(state, next_state, action) # the probability to get to next_state from state with action
+                            v_func_prev = value_function[next_state[0]][next_state[1]]
+                            inner_sum += p_s_sp_a * v_func_prev
+                        
+                        policy_a_s = self.policy(self.grid, action, state)  # if you are at state state, probability you take action action
+                        reward_s_a = self.grid.reward(action, state) # the reward if I take action action in state state
+                        gama = self.grid.gama
+                        v_s += policy_a_s * (reward_s_a + gama * inner_sum)
+
+                    value_function[state[0]][state[1]] = v_s
+
+
+
+
+
+
+
+                    
+
+        
